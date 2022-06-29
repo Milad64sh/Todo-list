bd9f4523ca1241f342e07e469295499cff12d904
Milad64sh <m.shalikarian@gmail.com>
2022-06-29 16:32:02 -0700

    second commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <div>
+        <h4>hello from TodoInput</h4>
+      </div>
+    );
+  }
+}
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h5>hello from TodoItem</h5>
+      </div>
+    );
+  }
+}
