7b03509f1c575cf36c6920a55a7549487b960305
Milad64sh <m.shalikarian@gmail.com>
2022-06-30 10:55:35 -0700

    seventh commit

--- src/App.js
+++ src/App.js
@@ -23,31 +23,42 @@ export default class App extends Component {
     };
     const updatedItems = [...this.state.items, newItem];
     this.setState({
+      items: updatedItems,
       item: '',
       id: uuid(),
-      items: updatedItems,
       editItem: false,
     });
   };
   clearList = () => {
-    console.log('clear list');
+    this.setState({
+      items: [],
+    });
   };
   handleDelete = (id) => {
-    console.log(`handle delete ${id}`);
+    const filterItems = this.state.items.filter((item) => item.id !== id);
+    this.setState({
+      items: filterItems,
+    });
   };
   handleEdit = (id) => {
-    console.log(`handle edit ${id}`);
+    const filterItems = this.state.items.filter((item) => item.id !== id);
+    const selectedItem = this.state.items.find((item) => item.id === id);
+    this.setState({
+      items: filterItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true,
+    });
   };
 
   render() {
-    console.log(this.state);
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-10 mx-auto col-md-8 mt-5'>
             <h3 className='text-capitalize text-center'>todo input</h3>
             <TodoInput
-              item={this.item}
+              item={this.state.item}
               handleChange={this.handleChange}
               handleSubmit={this.handleSubmit}
               editItem={this.state.editItem}
