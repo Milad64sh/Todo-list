5d030b875648e1ca63b9041666be8dba70c0bcaf
Milad64sh <m.shalikarian@gmail.com>
2022-06-30 08:35:49 -0700

    fourth commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 export default class App extends Component {
   state = {
-    items: [
-      { id: 1, title: 'wake up' },
-      { id: 2, title: 'make breakfast' },
-    ],
+    items: [],
     id: uuid(),
     item: '',
     editItem: false,
   };
   handleChange = (e) => {
-    console.log('handle change');
+    this.setState({
+      item: e.target.value,
+    });
   };
   handleSubmit = (e) => {
-    console.log('handle submit');
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItems = [...this.state.items, newItem];
+    this.setState({
+      item: '',
+      id: uuid(),
+      items: updatedItems,
+      editItem: false,
+    });
   };
   clearList = () => {
     console.log('clear list');
