7b0a0660adb99a723a52820d370093cd6cb36952
Milad64sh <m.shalikarian@gmail.com>
2022-06-30 08:50:53 -0700

    fifth commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 export default class TodoItem extends Component {
   render() {
-    return (
-      <div>
-        <h5>hello from TodoItem</h5>
-      </div>
-    );
+    return;
   }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 import Item from './TodoItem';
 
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <div>
-        <h3>hello from TodoList</h3>
-      </div>
+      <ul className='list-group my-5'>
+        <h3 className='text-capitalize text-center'>todo list</h3>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+        <div className='d-grid'>
+          <button
+            type='button'
+            className='btn btn-danger mt-5 tex t-uppercase'
+            onClick={clearList}
+          >
+            clear list
+          </button>
+        </div>
+      </ul>
     );
   }
 }
